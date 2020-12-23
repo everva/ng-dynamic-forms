f2392c454c9c9d471b4be38752dc9a8fb7443bd6
Atakan Atik <atknatk@gmail.com>
2020-12-23 16:58:26 +0300

    fix for multiple

--- publish.sh
+++ publish.sh
@@ -7,6 +7,6 @@ npm publish ./dist/ng-dynamic-forms/ui-foundation/
 npm publish ./dist/ng-dynamic-forms/ui-ionic/
 npm publish ./dist/ng-dynamic-forms/ui-kendo/
 npm publish ./dist/ng-dynamic-forms/ui-material/
-npm publish ./dist/ng-dynamic-forms/ui-ng-bootstrap/
+npm publish --access public ./dist/ng-dynamic-forms/ui-ng-bootstrap/
 npm publish ./dist/ng-dynamic-forms/ui-ngx-bootstrap/
 npm publish ./dist/ng-dynamic-forms/ui-primeng/
